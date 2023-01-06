8dc49d900b6b2a272a06c9d719e30a92dfa8822d
dvebino <dhuang2495@gmail.com>
2023-01-06 15:45:26 -0500

    Hero section text added and styled

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I'm David<br />
+        Welcome To My Portfolio
+      </SectionTitle>
+      <SectionText>
+      Crafting visually stunning and user-friendly web experiences with passion and precision.
+      </SectionText>
+
+      <Button onClick={() => window.location = "https://google.com"}>Read More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
