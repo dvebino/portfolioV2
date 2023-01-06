281fd2e5e59e6d797cf3e72a9cc9204ed6acb671
dvebino <dhuang2495@gmail.com>
2023-01-06 14:44:03 -0500

    header tweaked for logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
-
+import { AiFillGithub, AiFillGoogleCircle, AiFillLinkedin } from 'react-icons/ai';
+import { FaConnectdevelop } from 'react-icons/fa';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
@@ -10,8 +10,8 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{display: "flex", alignItems: "center", color:"white"}}>
-          <DiCssdeck size="3rem"/>
-          <span> Portfolio V2 </span>
+          <FaConnectdevelop size="3rem"/>
+          <span style={{marginLeft:"1rem"}}>Portfolio V2 </span>
         </a>
       </Link>
     </Div1>
@@ -38,6 +38,18 @@ const Header = () =>  (
         </Link>
       </li>
     </Div2> 
+
+    <Div3>
+      <SocialIcons href="https://github.com/dvebino">
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com/in/dhuang24">
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="mailto:dhuang2495@gmail.com">
+        <AiFillGoogleCircle size="3rem"/>
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
