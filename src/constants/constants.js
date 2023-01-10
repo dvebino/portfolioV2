5fb7f5ff46ba2b1de5319679ae504fff084f87eb
dvebino <dhuang2495@gmail.com>
2023-01-10 15:14:43 -0500

    adding submit functionality to contact form

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'BoBo Movie App',
     description: `An iOS Mobile Application with the objective of creating a social application to deliver weighted
     recommendations based upon user’s data. The app incorporates multi-variable functionality to fine-tune results
-    based upon streaming services, an user's watch history, and genre preferences.`,
+    based upon streaming services, an user's watch history, and genre preferences. An easy-to-use application to finally solve the most important question: "What should we watch tonight?"`,
       image: '/images/Bobo.png',
       tags: ['PostgreSQL','Express','React Native','Node'],
     source: 'https://google.com',
@@ -12,7 +12,7 @@ export const projects = [
   },
   {
     title: 'Team Dashboard',
-    description: `Built a dynamic web application to provide team managers easy access to business analytics, transactions, and various integrated project management features such as a Kanban board.`,
+    description: `Built a dynamic web application to provide team managers easy access to business analytics, transactions, and various integrated project management features such as a Kanban board. The web application supports the various needs of a team and presents it in a clean and concise user interface.`,
       image: '/images/team-dash.png',
       tags: ['React', 'TailwindCSS', 'JavaScript', 'Syncfusion'],
     source: 'https://google.com',
@@ -21,7 +21,7 @@ export const projects = [
   },
   {
     title: 'Social_py',
-    description:"Developed an Instagram Clone utilizing Python and Django to support the Full Stack Application.",
+    description:"Developed an Instagram Clone utilizing Python and Django to support the Full Stack Application. Django's clean design and toolkit allowed quick access and easy ability to recieve or alter information on the database.",
     image: '/images/social-py.jpg',
     tags: ['Python','Django'],
     source: 'https://google.com',
@@ -30,7 +30,7 @@ export const projects = [
   },
   {
     title: 'DJ Landing Page',
-    description: "Built a landing page for NY-Local DJ (DVSM) to support their fanbase. Incorporated a built-in music player for users to listen to their latest hits.",
+    description: "Built a landing page for NY-Local DJ (DIMSVM) to support their fanbase. Incorporated a built-in music player for users to listen to their latest hits. Utilized Next.js to build our React-based web application with server-side rendering.",
     image: '/images/dj.png',
     tags: ['Next', 'React', 'JavaScript'],
     source: 'https://google.com',
@@ -39,7 +39,7 @@ export const projects = [
   },
   {
     title: 'Medium Clone - Blog Page',
-    description: "Developed an Medium Clone",
+    description: "",
       image: '/images/mediumClone.png',
       tags: ['React', 'TypeScript', 'TailwindCSS'],
     source: 'https://google.com',
